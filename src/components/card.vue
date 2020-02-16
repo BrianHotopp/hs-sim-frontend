9c26c3acda67b33fac0e829cdcfb505c159b7f5e
brianhotopp <brihoto@gmail.com>
2020-02-16 02:17:31 -0500

    added search feature with cards

---
+++ src/components/card.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="card">
+  <div class="image-wrapper">
+  <img :src="this.aCard.battlegrounds.image" class="card-img-top"
+   alt="oops! no image to display">
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">
+      {{ this.aCard.cardSet == 'Battlegrounds' ? 'Golden':'' }} {{this.aCard.name}}
+    </h5>
+    <p class="card-text">{{this.aCard.race}}</p>
+    <a href="#" class="btn btn-primary">Play Me</a>
+  </div>
+</div>
+</template>
+<script>
+export default {
+  name: 'card',
+  props: ['aCard'],
+};
+</script>
+<style scoped>
+.card {
+  max-width: 14rem;
+  height: 100%;
+}
+.image-wrapper {
+  height:10rem;
+  overflow: hidden;
+}
+img{
+  width:100%;
+  height: 100%;
+  object-fit: none;
+  object-position: 50% 20%;
+}
+</style>
---
+++ src/components/search.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <form v-on:submit.prevent="onSubmit">
+      <input v-model="search_term" placeholder="enter search term">
+      {{this.search_term}}
+    </form>
+  <b-container>
+    <b-row v-if="this.results">
+        <b-col cols = "3" v-for="result in this.results" :key = result.id>
+          <card :aCard = result></card>
+        </b-col>
+    </b-row>
+  </b-container>
+  </div>
+</template>
+<script>
+import axios from 'axios';
+import card from './card.vue';
+
+const blizzardAPI = axios.create({
+  baseURL: 'https://us.api.blizzard.com/hearthstone',
+  headers: {
+    Authorization: 'Bearer USy8jnFHfEiRMHCsuZLqZw1HXvhYZD6mW2',
+  },
+});
+export default {
+  name: 'search',
+  data() {
+    return {
+      search_term: '',
+      results: null, // this will contain the result of the user's search
+    };
+  },
+  components: {
+    card,
+  },
+  methods: {
+    onSubmit() {
+      // blizzardAPI.get('https://us.api.blizzard.com/hearthstone/cards?locale=en_US&gameMode=battlegrounds&tier=hero%2C3&textFilter=mech&access_token=USaHVhmVPLx4OBZCRJ4d333EiIRyXKC46e')
+      //   .then((response) => { console.log(response); });
+      blizzardAPI.get('/cards', {
+        params: {
+          textFilter: this.search_term,
+          locale: 'en_US',
+          gameMode: 'battlegrounds',
+        },
+      })
+        .then((response) => {
+          this.results = response.data.cards;
+        });
+      this.search_term = 'done';
+    },
+  },
+};
+</script>
+
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
